47be67285cce98659a28a442e20c9afe5962697f
Brian Kubes <briankubes@gmail.com>
2020-11-02 13:01:23 -0800

    server is up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const accountRouter = require('../accounts/accountRouter.js')
+// const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter)
+server.get('/', (req, res)=> {
+    res.send('working!!!')
+})
 
 module.exports = server;
